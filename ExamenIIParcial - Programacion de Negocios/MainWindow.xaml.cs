17b124b9f03096d76f0f32b7e9d691bfd00f9488
MrMRmiguelo <miguelovaldez01@gmail.com>
2019-07-08 13:25:45 -0600

    Diseño de Pantalla y agregar botones

--- ExamenIIParcial - Programacion de Negocios/MainWindow.xaml.cs
+++ ExamenIIParcial - Programacion de Negocios/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//NAMESPACE NECESARIOS
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
 
 namespace ExamenIIParcial___Programacion_de_Negocios
 {
@@ -20,9 +25,89 @@ namespace ExamenIIParcial___Programacion_de_Negocios
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlConnection;
         public MainWindow()
         {
             InitializeComponent();
+            SqlConnection sqlConnection = new SqlConnection();
+            SmtpClient correo = new SmtpClient();
         }
+        private void MostrarUsers()
+        {
+            
+           
+            
+                try
+                {
+                    string query = "SELECT * FROM Usuarios.Usuario";
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                    using (sqlDataAdapter)
+                    {
+                        DataTable Usuarios = new DataTable();
+                        sqlDataAdapter.Fill(Usuarios);
+
+                        lbUsuarios.DisplayMemberPath = "nombre";
+                        lbUsuarios.SelectedValuePath = "id";
+                        lbUsuarios.ItemsSource = Usuarios.DefaultView;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            
+        }
+
+
+        private void BtnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBUser.Text == string.Empty)
+            {
+                MessageBox.Show("Debe ingresar un nombre de usuario");
+                TBUser.Focus();
+            }
+            else
+            {
+                try
+                {
+                    string query = "INSERT INTO Usuarios.Usuario(nombre) VALUES(@nombre)";
+
+                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                    sqlConnection.Open();
+
+                    sqlCommand.Parameters.AddWithValue("@nombre", TBUser.Text);
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                    MostrarUsers();
+                }
+
+            }
+        }
+
+        private void BtnEditar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnModificar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+       
     }
 }
+
